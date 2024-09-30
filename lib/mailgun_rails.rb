1003242f50f284a6dfa24a99ce67744823949297
Nicolas Rodriguez <nico@nicoladmin.fr>
2024-09-30 03:15:52 +0200

    Coding style

--- lib/mailgun_rails.rb
+++ lib/mailgun_rails.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
+# require ruby dependencies
 require 'json'
 require 'ostruct'
 require 'openssl'
+
+# require external dependencies
 require 'active_support'
 require 'active_support/core_ext'
-
 require 'zeitwerk'
-loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
-loader.inflector.inflect 'mailgun-rails' => 'MailgunRails'
-loader.setup
+
+# load zeitwerk
+Zeitwerk::Loader.for_gem(warn_on_extra_files: false).tap do |loader|
+  loader.ignore("#{__dir__}/mailgun-rails.rb")
+  loader.setup
+end
 
 module MailgunRails
 end
